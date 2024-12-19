4c7835fa60d15743265f2598250c3fbeb0205ba2
Pchelovodova <pchelovodovaes@gmail.com>
2024-12-19 12:07:34 +0100

    Сортировка пузырьком. Работа над ошибками

--- bubble_sort.py
+++ bubble_sort.py
@@ -7,16 +7,18 @@ def bubble_sort(arr: list[int]) -> list[int]:
     :param arr: list[int]: Входящий массив значений для сортировки
     :return: list[int]: Отсортированный список
     """
-    left_element, right_element = 0, 1
-    steps = len(arr) - 1
-    while steps > 0:
-        while right_element < len(arr):
-            if arr[left_element] > arr[right_element]:
-                arr[left_element], arr[right_element] = arr[right_element], arr[left_element]
-            left_element += 1
-            right_element += 1
-        steps -= 1
-        left_element, right_element = 0, 1
+    i = 0
+    unchanged_index = 0
+    while unchanged_index < len(arr) - 1:
+        unchanged_index = 0
+        while i + 1 < len(arr):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+            else:
+                unchanged_index += 1
+            i += 1
+        i = 0
+
     return arr
 
 
@@ -27,3 +29,4 @@ my_list = [64, 34, 25, 12, 22, 11, 90]
 print(f'Список на входе: {my_list}')
 bubble_sort(my_list)
 print(f'Отсортированный список: {my_list}')
+
